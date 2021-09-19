82bebc2b5c6049ea75f33499fc7fd9e1b976c03d
LEONARDO MARANHAO FARIA DA SILVA <lmfaria@br.ibm.com>
2021-09-19 10:00:34 -0300

    split unit and integration tests

--- src/integration.test.ts
+++ src/integration.test.ts
@@ -2,8 +2,8 @@ import request from 'supertest';
 import app from './app';
 import MongoDBClient from './db/mongo-db-client';
 
-describe('integration testing', () => {
-  it('should ', async () => {
+describe('integration-test', () => {
+  it('should return correct results from the 2 dbs', async () => {
     const response = await request(app).get('/rides');
     expect(response.status).toBe(200);
     expect(response.body.length).toBe(17);
